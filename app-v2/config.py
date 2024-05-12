da2bdd489466039eaa0ce38644ef97223f633535
RodrigooBrann <166259658+RodrigooBrann@users.noreply.github.com>
2024-05-12 13:33:12 -0400

    Create config.py

---
+++ app-v2/config.py
@@ -0,0 +1,9 @@
+# config.py
+MYSQL_HOST = "35.199.102.147" # Substitua pelo IP público da sua instância
+MYSQL_DATABASE = "banco-dados-aplicativo"
+MYSQL_USER = "admin"
+MYSQL_PASSWORD = "m0x$n2v-CGM010ib"
+
+PROJECT_ID = 'YOUR_PROJECT_ID'
+ENDPOINT_NAME = 'YOUR_ENDPOINT_NAME'
+CREDENTIAL_PATH = 'path/to/your/credentials.json'
