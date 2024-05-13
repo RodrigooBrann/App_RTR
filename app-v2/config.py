280191bf7d5021f64a6283fa982bfc66d6692ac7
RodrigooBrann <166259658+RodrigooBrann@users.noreply.github.com>
2024-05-13 17:37:53 -0400

    Update config.py

--- app-v2/config.py
+++ app-v2/config.py
@@ -1,9 +1,11 @@
 # config.py
 MYSQL_HOST = "35.199.102.147" # Substitua pelo IP público da sua instância
-MYSQL_DATABASE = "banco-dados-aplicativo"
+MYSQL_DATABASE = "default"
 MYSQL_USER = "admin"
-MYSQL_PASSWORD = "m0x$n2v-CGM010ib"
+MYSQL_PASSWORD = "123456"
 
 PROJECT_ID = 'YOUR_PROJECT_ID'
 ENDPOINT_NAME = 'YOUR_ENDPOINT_NAME'
 CREDENTIAL_PATH = 'path/to/your/credentials.json'
+
+GOOGLE_API_KEY = "AIzaSyDumNZWD3njWLg32ZPxq2gBagz7w88mPxE"
